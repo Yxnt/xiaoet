1dde5421778e10b685a9cc6609c0ee0b1df307d8
Yxn <j__xnt@163.com>
2018-10-08 16:32:43 +0800

    Squashed commit of the following:
    commit a575a21d79a7053614a110ec29db71200d6acf8e
    Author: Yxn <j__xnt@163.com>
    Date:   Mon Oct 8 16:31:09 2018 +0800
    
        支持音频下载

--- xiaoet.py
+++ xiaoet.py
@@ -25,7 +25,7 @@ class XET(object):
         url = 'https://pc-shop.xiaoe-tech.com/{appid}/open/column.resourcelist.get/2.0'.format(appid=self.APPID)
         body = {
             'data[page_index]': '0',
-            'data[page_size]': '100',
+            'data[page_size]': '1000',
             'data[order_by]': 'start_at:desc',
             'data[resource_id]': self.RESOURCEID,
             'data[state]': '0'
@@ -35,16 +35,20 @@ class XET(object):
         if resp.status_code != 200:
             raise Exception('获取课程列表失败')
         try:
-            data = [{'id': lesson['id'], 'name': lesson['title']} for lesson in resp.json()['data']]
+            data = [{'id': lesson['id'], 'name': lesson['title'], 'resource_type': lesson['resource_type']} for lesson
+                    in resp.json()['data']]
         except Exception as e:
             print("获取课程列表失败")
             exit(1)
         return data
 
-    def get_lesson_hls(self, resourceid):
-        url = 'https://pc-shop.xiaoe-tech.com/{appid}/open/video.detail.get/1.0'.format(appid=self.APPID)
+    def get_lesson_hls(self, resource):
+        resource_type = {'2': 'audio.detail.get', '3': 'video.detail.get'}
+        url = 'https://pc-shop.xiaoe-tech.com/{appid}/open/{resource}/1.0'.format(appid=self.APPID,
+                                                                                  resource=resource_type[
+                                                                                      str(resource['resource_type'])])
         body = {
-            'data[resource_id]': resourceid
+            'data[resource_id]': resource['id']
         }
         self.header['Referer'] = 'https://pc-shop.xiaoe-tech.com/{appid}/video_details?id={resourceid}'.format(
             appid=self.APPID, resourceid=self.RESOURCEID)
@@ -54,55 +58,89 @@ class XET(object):
         hls = resp.json()['data']
         return hls
 
+    def video(self, url, media_dir, title, playurl):
+        resp = self.session.get(url, headers=self.header)
+
+        media = loads(resp.text)
+        playlist = ["{playurl}{uri}".format(playurl=playurl, uri=uri) for uri in media.segments.uri]
+
+        n = 0
+        new_segments = []
+        # get ts file list
+        for url in playlist:
+            ts_file = os.path.join(media_dir, title, 'm_{num}.ts'.format(num=n))
+            ts_path = os.path.join(title, 'm_{num}.ts'.format(num=n))
+            media.data['segments'][n]['uri'] = ts_path
+            new_segments.append(media.data.get('segments')[n])
+            resp = self.session.get(url, headers=self.header, cookies=self.cookie)
+            if resp.status_code != 200:
+                print('Error: {title} {tsfile}'.format(title=title, tsfile=ts_file))
+
+            if not os.path.exists(ts_file) or os.stat(ts_file).st_size != resp.headers['content-length']:
+                with open(ts_file, 'wb') as ts:
+                    ts.write(resp.content)
+
+            n += 1
+
+        # change m3u8 data
+        media.data['segments'] = new_segments
+        segments = SegmentList(
+            [Segment(base_uri=None, keyobject=find_key(segment.get('key', {}), media.keys), **segment)
+             for segment in
+             media.data.get('segments', [])])
+        media.segments = segments
+
+        # save m3u8 file
+        m3u8_file = os.path.join(media_dir, '{title}.m3u8'.format(title=title))
+        if not os.path.exists(m3u8_file):
+            with open(m3u8_file, 'wb', encoding='utf8') as f:
+                f.write(media.dumps())
+
+    def audio(self, url, media_dir, title):
+        # 下载音频
+        resp = self.session.get(url, headers=self.header, stream=True)
+        if resp.status_code != 200:
+            print('Error: {title}'.format(title=title))
+        else:
+            audio_file = os.path.join(media_dir, title, '{title}.mp3'.format(title=title))
+            if not os.path.exists(audio_file):
+                with open(audio_file, 'wb') as f:
+                    f.write(resp.content)
+
     def download(self):
         media_dir = 'media'
         for resourceid in self.get_lesson_list():
-            data = self.get_lesson_hls(resourceid['id'])
-            url = data['video_hls']
-            title = data['title']
-            playurl = url.split('v.f230')[0]
 
-            # mkdir media directory
+            if resourceid['resource_type'] == 1 or resourceid['resource_type'] == 6:
+                continue
+
+            data = self.get_lesson_hls(resourceid)
+            title = data['title']
+
             if not os.path.exists(media_dir):
                 os.mkdir(media_dir)
 
-            if not os.path.exists(os.path.join(media_dir, title)):
-                os.mkdir(os.path.join(media_dir, title))
+            if resourceid['resource_type'] == 2:
+                playurl = data['audio_url']
 
-            resp = self.session.get(url, headers=self.header)
-            media = loads(resp.text)
-            playlist = ["{playurl}{uri}".format(playurl=playurl, uri=uri) for uri in media.segments.uri]
+                if not os.path.exists(os.path.join(media_dir, title)):
+                    try:
+                        os.mkdir(os.path.join(media_dir, title))
+                    except OSError as e:
+                        title = title.replace('|', '丨')
+                        os.mkdir(os.path.join(media_dir, title))
+                self.audio(playurl, media_dir, title)
 
-            n = 0
-            new_segments = []
-            # get ts file list
-            for url in playlist:
-                ts_file = os.path.join(media_dir, title, 'm_{num}.ts'.format(num=n))
-                ts_path = os.path.join(title, 'm_{num}.ts'.format(num=n))
-                media.data['segments'][n]['uri'] = ts_path
-                new_segments.append(media.data.get('segments')[n])
-                if not os.path.exists(ts_file):
-                    resp = self.session.get(url, headers=self.header, cookies=self.cookie)
-                    if resp.status_code != 200:
-                        print('Error: {title} {tsfile}'.format(title=title,tsfile=ts_file))
-                    else:
-                        with open(ts_file, 'wb') as ts:
-                            ts.write(resp.content)
-                n += 1
+            elif resourceid['resource_type'] == 3:
+                url = data['video_hls']
+                playurl = url.split('v.f230')[0]
 
-            # change m3u8 data
-            media.data['segments'] = new_segments
-            segments = SegmentList(
-                [Segment(base_uri=None, keyobject=find_key(segment.get('key', {}), media.keys), **segment)
-                 for segment in
-                 media.data.get('segments', [])])
-            media.segments = segments
+                # mkdir media directory
+                if not os.path.exists(os.path.join(media_dir, title)):
+                    os.mkdir(os.path.join(media_dir, title))
 
-            # save m3u8 file
-            m3u8_file = os.path.join(media_dir, '{title}.m3u8'.format(title=title))
-            if not os.path.exists(m3u8_file):
-                with open(m3u8_file, 'w', encoding='utf8') as f:
-                    f.write(media.dumps())
+                self.video(url, media_dir, title, playurl)
+
 
 if __name__ == '__main__':
     XET().download()
